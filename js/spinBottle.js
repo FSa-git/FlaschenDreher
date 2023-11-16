cbcbfbcd858c935eaffb67cd3a4b25ca0b03b1c8
FS <francesco9721@gmail.com>
2023-11-16 22:49:43 +0100

    this version works too

--- js/spinBottle.js
+++ js/spinBottle.js
@@ -1,24 +1,26 @@
 console.log("spinBottle.js loaded");
 
-function spinBottle(bottleId) {
-    console.log("Bottle spun!");
+// function spinBottle(bottleId) {
+//     console.log("Bottle spun!");
 
-    // Get the bottle element
-    let bottle = document.getElementById(bottleId);
-    bottle.style.transition = 'transform 2s ease-in-out';
+//     // Get the bottle element
+//     let bottle = document.getElementById(bottleId);
+//     bottle.style.transition = 'transform 2s ease-in-out';
 
-    // Casual rotation between 720 and 1080 degrees
-    let initialRotation = Math.floor(Math.random() * 720) + 1080;
-    bottle.style.transform = 'rotate(' + initialRotation + 'deg)';
+//     // Casual rotation between 720 and 1080 degrees
+//     let initialRotation = Math.floor(Math.random() * 720) + 1080;
+//     bottle.style.transform = 'rotate(' + initialRotation + 'deg)';
 
-    // After the animation is done, remove the transition and add the new class
-    setTimeout(function () {
-        let rotationDegrees = Math.floor(Math.random() * 360) + 720;
-        bottle.style.transform = 'rotate(' + rotationDegrees + 'deg)';
+//     // After the animation is done, remove the transition and add the new class
+//     setTimeout(function () {
+//         let rotationDegrees = Math.floor(Math.random() * 360) + 720;
+//         bottle.style.transform = 'rotate(' + rotationDegrees + 'deg)';
 
-        setTimeout(function () {
-            bottle.style.transition = '';
-        }, 2000);
+//         setTimeout(function () {
+//             bottle.style.transition = '';
+//         }, 2000);
 
-    }, 200);
-}
+//     }, 200);
+// }
+
+

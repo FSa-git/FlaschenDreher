91ab95d4dca3c9b718fff5d5e7f600455f2cf145
FS <francesco9721@gmail.com>
2023-11-22 23:32:47 +0100

    added a language tool

---
+++ js/selectLangguage.js
@@ -0,0 +1,24 @@
+/** JS-File to select the language */
+
+function changeLanguage(languageCode) {
+
+if (languageCode === 'en'){
+    document.getElementById('homeTitle').innerText = 'Spin the bottle!';
+    document.getElementById('yourTurn').innerText = 'Your turn!';
+    document.getElementById('spinButton').innerText = 'Spin';
+    document.getElementById('aboutText').innerText = 'About';
+}
+else if (languageCode === 'de') {
+    document.getElementById('homeTitle').innerText = 'Flaschendrehen!';
+    document.getElementById('yourTurn').innerText = 'Du bist dran!';
+    document.getElementById('spinButton').innerText = 'Dreh die Flasche';
+    document.getElementById('aboutText').innerText = 'Über das Projekt';
+}
+else if (languageCode === 'it') {
+    document.getElementById('homeTitle').innerText = 'Gira la bottiglia!';
+    document.getElementById('yourTurn').innerText = 'È il tuo turno';
+    document.getElementById('spinButton').innerText = 'Gira';
+    document.getElementById('aboutText').innerText = 'Il progetto';
+}
+
+}
